0b457a2a03377bacbfc37c486a33f305b0c2144d
rviry <rom88.viry@gmail.com>
2023-10-15 15:10:32 +0200

    build(poetry): move from pipenv to poetry

--- boot.sh
+++ boot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-uwsgi --http :8000 --gevent 1000 --http-websockets --master --wsgi-file wsgi.py --callable app
+uwsgi --http :8000 --gevent 1000 --http-websockets --master --wsgi-file wsgi_v2.py --callable app
